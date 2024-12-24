050317ca31dc29984d69e3e4724f052a4427d177
zhaoyukoon <yinggong.zhao@gmail.com>
2024-12-24 22:37:46 +0800

    support re-try

--- example_script.py
+++ example_script.py
@@ -25,9 +25,13 @@ def translate_text(text, index):
 
 
 def translate_multiple_thread(text_splits, max_workers=4):
-    with tqdm(total=len(text_splits), desc="Translate text") as pbar:
+    remained_splits = []
+    for i, text in enumerate(text_splits):
+        if i not in global_result:
+            remained_splits.append([i, text])
+    with tqdm(total=len(remained_splits), desc="Translate text") as pbar:
         with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-            future_to_translation = {executor.submit(translate_text, text, i): (i, text) for i, text in enumerate(text_splits)}
+            future_to_translation = {executor.submit(translate_text, pair[1], pair[0]): pair for pair in remained_splits}
             # 提交每个问题的处理
             for future in concurrent.futures.as_completed(future_to_translation):
                 (index, text) = future_to_translation[future]
@@ -37,17 +41,10 @@ def translate_multiple_thread(text_splits, max_workers=4):
                     print(f'Translate {text} throw an exception: {exc}')
                 finally:
                     pbar.update(1)
-
-if __name__ == "__main__":
-    source_lang, target_lang, country = "Chinese", "English", "China"
-
-    file_path = "examples/sample-texts/chinese-short.txt"
-    file_path = '九州·斛珠夫人.chapt1.txt'
+    return len(global_result) == len(text_splits)
 
 
-    with open(file_path, encoding="utf-8") as file:
-        source_text = file.read()
-
+def split_novel(source_text):
     splits = re.split(r'(\n\n第.章)', source_text.replace(u'\u3000', ' '))
     chapters = []
     current = ''
@@ -58,11 +55,31 @@ if __name__ == "__main__":
         else:
             current = current + split
     chapters += split_text(current, 300)
+    return chapters
 
-    start = time.time()
-    translate_multiple_thread(chapters)
-    end = time.time()
-    logger.info(f'Translation done, cost {end-start} seconds.')
+
+if __name__ == "__main__":
+    source_lang, target_lang, country = "Chinese", "English", "China"
+
+    file_path = '九州·斛珠夫人.chapt1.txt'
+    with open(file_path, encoding="utf-8") as file:
+        source_text = file.read()
+
+    chapters = split_novel(source_text)
+
+    tries = 0
+    MAX_TRIES = 5
+    while True:
+        logger.info(f'try {tries} time')
+        start = time.time()
+        status = translate_multiple_thread(chapters)
+        end = time.time()
+        logger.info(f'Translation done, cost {end-start} seconds. status:{status}')
+        if status:
+            break
+        tries += 1
+        if tries >= 4:
+            logger.info('exceeed MAX_TRIES {MAX_TRIES}, break')
     with open('init.txt', 'w') as f_init, open('reflection.txt', 'w') as f_ref, open('final.txt', 'w') as f_final:
         for key in sorted(global_result):
             init = global_result[key][0]
